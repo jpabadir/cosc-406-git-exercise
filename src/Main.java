a76cb256d423fc58595ea9c86b9bb4b16aff6efe
Jean-Philippe Abadir <jpabadir@gmail.com>
2020-09-16 13:20:30 +0200

    Implement summation and add tests

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,15 @@
 public class Main {
-    int[] inputArray = { 1, 4, 5, 3, 5, 9, 8, 5 };
+    static int[] inputArray = { 1, 4, 5, 3, 5, 9, 8, 5 };
 
     public static void main(String[] args) {
-        
+        System.out.println(getSum(inputArray));
+    }
+    
+    public static int getSum(int[] input) {
+        int result = 0;
+        for (int i : input) {
+            result += i;
+        }
+        return result;
     }
 }
